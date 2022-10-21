bbfb20ad23863230a3f7e01fb35a489e75dc950e
yato <me@oha.moe>
2022-10-21 15:09:29 +0800

    feat: remove some modules

--- src/main/java/com/web/web2022/controller/TopUpPlanController.java
+++ src/main/java/com/web/web2022/controller/TopUpPlanController.java
@@ -5,12 +5,16 @@ import com.web.web2022.model.outbound.OutBoundTopUpPlan;
 import com.web.web2022.model.outbound.OutBoundUser;
 import com.web.web2022.service.TopUpPlanService;
 import com.web.web2022.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/top_up_plans")
@@ -22,13 +26,9 @@ public class TopUpPlanController {
 
     @GetMapping
     public List<OutBoundTopUpPlan> find() {
-        return topUpPlanService.find().parallelStream()
-            .map(OutBoundTopUpPlan::new)
-            .collect(Collectors.toList());
     }
 
     @PostMapping("/{id}/top_up")
     public OutBoundUser topUp(@PathVariable Integer id, @AuthenticationPrincipal User user) {
-        return new OutBoundUser(userService.topUp(user, id));
     }
 }
--- src/main/java/com/web/web2022/service/impl/TopUpPlanServiceImpl.java
+++ src/main/java/com/web/web2022/service/impl/TopUpPlanServiceImpl.java
@@ -3,6 +3,7 @@ package com.web.web2022.service.impl;
 import com.web.web2022.model.domain.TopUpPlan;
 import com.web.web2022.repo.TopUpPlanRepo;
 import com.web.web2022.service.TopUpPlanService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,5 @@ public class TopUpPlanServiceImpl implements TopUpPlanService {
 
     @Override
     public List<TopUpPlan> find() {
-        return topUpPlanRepo.find();
     }
-
 }
