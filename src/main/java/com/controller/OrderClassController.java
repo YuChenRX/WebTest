01869de8a01cc6e6ce0aac0feb4ecbae552a716f
yuchenrx <3225393887@qq.com>
2022-11-14 08:44:10 +0800

    test

--- src/main/java/com/controller/OrderClassController.java
+++ src/main/java/com/controller/OrderClassController.java
@@ -4,10 +4,7 @@ import com.result.Result;
 import com.result.ResultFactory;
 import com.service.OrderClassService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
@@ -17,6 +14,7 @@ public class OrderClassController {
     OrderClassService service;
 
     @GetMapping("/{id}")
+    @CrossOrigin
     public Result findByUserId(@PathVariable int id) {
         return ResultFactory.buildSuccessResult(service.getOrderClassByUserId(id));
     }
